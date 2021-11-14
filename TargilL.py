2955f86800d2028cb6a5c8a3c52842f04fe6b511
Batelweiss <weissbatell@gmail.com>
2021-11-14 12:48:59 +0200

    All

--- TargilL.py
+++ TargilL.py
@@ -4,27 +4,12 @@ import string
 
 # Questions 1,2: 
 def counter(Text):
-  
-    # variable to store total word count
     num_words = 0
-      
-    # variable to store total line count
     num_lines = 0
       
-      
-    # opening file using with() method
-    # so that file gets closed 
-    # after completion of work
     with open("TText.txt", 'r') as f:
-          
-        # loop to iterate file
-        # line by line
         for line in f:
-              
-            # incrementing value of 
-            # num_lines with each 
-            # iteration of loop to
-            # store total line count 
+         
             num_lines += 1
               
             # declaring a variable word
@@ -42,9 +27,7 @@ def counter(Text):
                 # that the encountered character
                 # is not white space and a word
                 if (letter != ' ' and word == 'Y'):
-                      
-                    # incrementing the word
-                    # count by 1
+    
                     num_words += 1
                       
                     # assigning value N to 
@@ -58,20 +41,13 @@ def counter(Text):
                 # is a white space
                 elif (letter == ' '):
                       
-                    # incrementing the space
-                    # count by 1
-                    # num_spaces += 1
-                      
                     # assigning value Y to
                     # variable word because after
                     # white space a word
                     # is supposed to occur
                     word = 'Y'
     
-       # printing total line count
-    print("1.Number of lines in text file: ", num_lines)
-    
-    # printing total word count 
+    print("1.Number of lines in text file: ", num_lines) 
     print("2.Number of words in text file: ", num_words)
       
 # Driver Code: 
@@ -85,15 +61,10 @@ if __name__ == '__main__':
 
 
 
-# Questions 3,5:
-# Open the file in read mode
+# Questions 3,5,8:
 text = open("TText.txt", "r")
 counter = 0
-  
-# Create an empty dictionary
 d = dict()
-  
-# Loop through each line of the file
 for line in text:
     # Remove the leading spaces and newline character
     line = line.strip()
@@ -110,39 +81,31 @@ for line in text:
   
     # Iterate over each word in line
     for word in words:
-        # wword = re.sub('[-,;:".!(){-}/?]+', '', word)
-        # Check if the word is already in dictionary
         if word in d:
-            # Increment count of word by 1
             d[word] = d[word] + 1
         else:
-            # Add the word to dictionary with count 1
             d[word] = 1
 # 8888888888888888888888888888888888888888888888888888888888888
 # Question 8:
-# colors={}
-# for key, val in d.items():
+# for key, value in d.items():
 #     if key in mc.cnames:
-#       print(key,':',val)
+#       print(key,':',value)
 # 8888888888888888888888888888888888888888888888888888888888888
 
-# Print the contents of dictionary
-# for key in list(d.keys()):
-#     print(key, ":", d[key])
 for key in list(d.keys()):
     if(d[key]==1):
         counter = counter + 1
 print("3.The amount of unique words in the file:",counter)
-z = d.values()
-Maxi_word = max(z)
+Thesumofeachword = d.values()
+Maxi_word = max(Thesumofeachword)
 for key in d:
     if(d[key]==Maxi_word):
         print("5.a.The most popular word in the text is: ",key)
 for key in list(d.keys()):
-    if(key == 'am' or key == 'are' or key == "don't" or key == 'that' or key == 'the' or key == 'it' or key == 'was' or key == 'of'  or key == 'to'  or key == 'a'  or key == 'and'  or key == 'in' or key == '' or key == 'i' or key == 'I'):
+    if(key == 'am' or key == 'are' or key == "don't" or key == 'that' or key == 'the' or key == 'it' or key == 'was' or key == 'of'  or key == 'to'  or key == 'a'  or key == 'and'  or key == 'in' or key == '' ):
        del(d[key])
-p = d.values()
-Maxi_word2 = max(p)
+Thesumofeachword2 = d.values()
+Maxi_word2 = max(Thesumofeachword2)
 for key in d:
     if(d[key]==Maxi_word2):
        print("5.b.The popular word Most that is not a word with syntactic meaning: ",key)
@@ -160,19 +123,8 @@ for i, value in enumerate(splitFile):
     sumOfAllLinesLength += length
     if length > maxLength:
         maxLength = length
-print("max length", maxLength)
-print("avg", sumOfAllLinesLength / len(splitFile))
-
-# for index, line in enumerate(text):   
-#      d[index] = len(line.split())
-# sumOfAllLinesLength = 0
-# for key in d:
-#     sumOfAllLinesLength = sumOfAllLinesLength + d[key]
-# z = d.values()
-# Maximum_length = max(z)
-# Average = sumOfAllLinesLength / (len(d))
-# print("4.a. Average sentence length in text: ", Average)
-# print("4.b. Maximum sentence length in text: ", Maximum_length)
+print("4.a. Average sentence length in text:", sumOfAllLinesLength / (len(splitFile)-1))
+print("4.b. Maximum sentence length in text:", maxLength)
 
 # Question 8:
 print("8. is written here in a comment. It works at GDB Online")
