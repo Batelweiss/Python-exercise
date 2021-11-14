6709963d348763d08253c244c366666395cf6158
Batelweiss <weissbatell@gmail.com>
2021-11-14 13:01:53 +0200

    All

--- TargilL.py
+++ TargilL.py
@@ -128,6 +128,16 @@ print("4.b. Maximum sentence length in text:", maxLength)
 
 # Question 8:
 print("8. is written here in a comment. It works at GDB Online")
+ 
+# Answers:
+# 1.Number of lines in text file:  640078
+# 2.Number of words in text file:  5775980
+# 3.The amount of unique words in the file: 34566
+# 5.a.The most popular word in the text is:  the
+# 5.b.The popular word Most that is not a word with syntactic meaning:  i
+# 4.a. Average sentence length in text: 22.67132303126109
+# 4.b. Maximum sentence length in text: 957
+# 8. is written here in a comment. It works at GDB Online
 
 
 
